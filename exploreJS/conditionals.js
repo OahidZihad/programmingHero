e2af56c0baec40b7ffe835dcd9b059a86c1b2099
OahidZihad <oahidzihad1@gmail.com>
2021-01-23 01:37:46 +0600

    Getting Started With JavaScript

---
+++ exploreJS/conditionals.js
@@ -0,0 +1,20 @@
+var biscuitPrice = 12;
+if(biscuitPrice <= 12){
+    console.log("I will buy the biscuit.");
+}
+else{
+console.log("Only Cha is better");
+}
+
+var jobPaiso = true;
+var takaJomse = 50000;
+if(jobPaiso == false && takaJomse > 20000){
+    console.log("You can marry.")
+}
+else if(jobPaiso == true){
+    console.log("wait few days.");
+}
+else{
+    console.log("You don't");
+}
+
---
+++ exploreJS/hello.js
@@ -0,0 +1,99 @@
+console.log(51);
+console.log('Good Morning');
+
+var bananaPrice = 12; 
+console.log(bananaPrice);
+
+var seenAfter = 21;
+console.log(typeof seenAfter);
+
+var name = "oahid zihad";
+console.log(typeof name);
+
+var isHot = true;
+var isRich = false;
+console.log(typeof isHot);
+
+var naame = "salam";
+console.log(typeof(naame));
+
+var myName = 'Hero Alom';
+myName = "Oahid Zihad";
+myName = "Jhankar"
+console.log(myName);
+
+var nameIs = `kdslajf`;
+var promise = "I promise I will work HARD to become a programmer";
+console.log(promise.toLowerCase());
+console.log(promise.toUpperCase());
+console.log(promise.indexOf('will'));
+console.log(promise.indexOf('a'));
+console.log(promise.split("I"));
+
+var number1 = 25;      //integer
+var number2 = "15.5";    //float
+number2 = parseFloat(number2);
+console.log(number1 + number2);
+
+
+var number1 = 25.5;
+var number2 = 15;    
+number1 = parseInt(number1);
+console.log(number1 + number2);
+
+
+var number1 = 25;      //integer
+var number2 = "15.5";    //float
+number2 = +number2;
+console.log(number1 + number2);
+
+var number1 = 25;      //integer
+var number2 = 15.5    //float
+number2 = ""+number2;
+console.log(number1 + number2);
+console.log(typeof number2);
+
+var number1 = 0.1;
+var number2 = 0.2;
+var total = number1 + number2;
+total = total.toFixed(3);
+console.log(total);
+
+var price1 = 25;
+var price2 = 35;
+var total = price1 % price2;
+price2--;
+console.log(price2);
+
+var a = 25;
+var b = 'alo';
+var result = a + b;
+console.log(result);
+
+var a = 'Justin';
+var b = 'Bieber';
+var result = a + ' ' + b;
+console.log(result);
+
+var a = -5;
+var absolute_a = Math.abs(a);
+console.log(absolute_a);
+
+var a = 5.4545;
+var round_a = Math.round(a);
+console.log(round_a);
+
+var a = 5.00009;
+var round_a = Math.ceil(a);
+console.log(round_a);
+
+var a = 5.999999;
+var round_a = Math.floor(a);
+console.log(round_a);
+
+var a = Math.random()*100;
+var result = Math.round(a);
+console.log(result);
+
+var date = new Date();
+console.log(date);
