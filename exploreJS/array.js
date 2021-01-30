8b8d1fc548b45afa4fb0dc248bfed169a41a66ae
OahidZihad <oahidzihad1@gmail.com>
2021-01-30 15:53:33 +0600

    Array advance, push, pop, length, shift, unshift

--- exploreJS/array.js
+++ exploreJS/array.js
@@ -1,5 +1,21 @@
 var friendsAge = [18, 20, 22, 30];
 var zihadAge = friendsAge[2];
 friendsAge[3] = 33;
-var position = friendsAge.indexOf(222);
+var position = friendsAge.indexOf(22);
 console.log(position);
+
+
+///Push, Pop & Length
+console.log(friendsAge);
+console.log(friendsAge.length);
+friendsAge.push(15);
+friendsAge.push(16);
+console.log(friendsAge);
+console.log(friendsAge.length);
+friendsAge.pop();
+console.log(friendsAge);
+console.log(friendsAge.length);
+friendsAge.unshift(11);
+console.log(friendsAge);
+friendsAge.shift();
+console.log(friendsAge);
