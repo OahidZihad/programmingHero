27d3b394453e4d53223a5ea7af5b96bedbc388bd
OahidZihad <oahidzihad1@gmail.com>
2021-01-31 01:26:01 +0600

    Switch-case, break, default

---
+++ exploreJS/switch-case.js
@@ -0,0 +1,15 @@
+var num = 100;
+switch(num){
+    case 1000:
+        console.log('I am 1000');
+        break;
+    case 100:
+        console.log("i am 100");
+        break;
+    case 20:
+    case 10:
+        console.log("I am either 10 or 20");
+        break;
+    default:
+        console.log("I don't know who are you");
+}
