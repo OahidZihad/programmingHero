768470d545defd0e7497409714a8477dc562d044
OahidZihad <oahidzihad1@gmail.com>
2021-01-31 01:55:45 +0600

    Function parameter, multiple parameter, function return

--- exploreJS/function.js
+++ exploreJS/function.js
@@ -4,3 +4,36 @@ function sayLoveYou(){
 }
 
 sayLoveYou();
+var date = 14;
+var place = "restaurant";
+sayLoveYou();
+var hangoutPlace = "TSC";
+sayLoveYou();
+console.log();
+
+function doubleIt(num){
+    var result = num * 2;
+    console.log(result);
+}
+
+console.log();
+doubleIt(10);
+
+function doubleAndAdd(num){
+    var result = num * 2;
+    return result;
+}
+
+var first = doubleAndAdd(10);
+var second = doubleAndAdd(20);
+var total = first+second;
+console.log(total);
+
+
+function add(num1, num2){
+    var result = num1 + num2;
+    return result;
+}
+var sum = add(10, 10);
+console.log(sum);
+
