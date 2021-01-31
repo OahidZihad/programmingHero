70fdd43a6a722ea959a662f9092684e5a6b9ac43
OahidZihad <oahidzihad1@gmail.com>
2021-01-31 17:14:31 +0600

    variable let and const and how to use them

---
+++ exploreJS/Apply JS Concept/constLet.js
@@ -0,0 +1,12 @@
+var name = "Zihad";
+if(name.length>4){
+    name = "Zihu";
+}
+console.log(name);
+
+let age = 15;  ///// let thaka mane, ei age k update kora jabe. Changeable.......
+age = 16;
+age = 17;
+console.log(age);
+const country = "Bangladesh"; //// const thaka mane, ei country k update kora jabe na. Not Changeable.
+console.log(country);
