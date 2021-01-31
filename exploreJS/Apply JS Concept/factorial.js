820eb58c86721520b6f4e99394c720071d47caca
OahidZihad <oahidzihad1@gmail.com>
2021-01-31 18:16:26 +0600

    Calculate Factorial of a number using a while loop

--- exploreJS/Apply JS Concept/factorial.js
+++ exploreJS/Apply JS Concept/factorial.js
@@ -1,3 +1,5 @@
+///// Using Function and For loop
+///// Using Function and For loop
 function factorial(num){
     var fact = 1;
     for(var i=1; i<=num; i++){
@@ -6,4 +8,20 @@ function factorial(num){
     return fact;
 }
 var factorial = factorial(5);
+console.log(factorial);
+
+
+////// Using Function and While Loop
+////// Using Function and While Loop
+function factorial2(num){
+    var i = 1;
+    var fact = 1;
+    while(i <= num){
+    fact = fact * i;
+    i++;
+}
+    return fact;
+}
+
+var factorial = factorial2(10);
 console.log(factorial);
