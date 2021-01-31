734d88a71127811d74ba35fe103c7e7741793793
OahidZihad <oahidzihad1@gmail.com>
2021-02-01 00:25:56 +0600

    Fibonacci Element in a Recursive Way

--- exploreJS/Apply JS Concept/fibonacci.js
+++ exploreJS/Apply JS Concept/fibonacci.js
@@ -1,9 +1,9 @@
-// var fibo = [0, 1];
-// // fibo[2] = fibo[]
-// for (var i=2; i<=10; i++){
-//     fibo[i] = fibo[i-1] + fibo[i-2];
-// }
-// console.log(fibo);
+////// iterative function
+var fibo = [0, 1];
+for (var i=2; i<=10; i++){
+    fibo[i] = fibo[i-1] + fibo[i-2];
+}
+console.log(fibo);
 
 
 function fibonacci(num){
@@ -16,3 +16,20 @@ function fibonacci(num){
 
 var fibonacci = fibonacci(10);
 console.log(fibonacci);
+
+
+////// recursive function
+function fibonacci1(num){
+    if(num==0){
+        return 0;
+    }
+    else if(num==1){
+        return 1;
+    }
+    else{
+        return fibonacci1(num - 1) + fibonacci1(num - 2);
+    }
+}
+
+var result = fibonacci1(10);
+console.log(result);
