860a04697a05f5892a47d5d610cba75890a7df0a
OahidZihad <oahidzihad1@gmail.com>
2021-01-31 17:02:44 +0600

    UNIT CONVERT INCH TO FEET USE VARIABLE AND FUNCTION

---
+++ exploreJS/Apply JS Concept/inchToFeet.js
@@ -0,0 +1,12 @@
+// var inch = 156;
+// var feet = inch/12;
+// console.log(feet);
+
+
+function inchToFeet(inch){
+    var feet = inch/12;
+    return feet;
+}
+
+var inchToFeet = inchToFeet(256);
+console.log(inchToFeet);
