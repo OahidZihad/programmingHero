348d27648d913b7849b5e6e41b744b1815ae58d6
OahidZihad <oahidzihad1@gmail.com>
2021-02-01 16:31:52 +0600

    Count the number of words in a string

---
+++ exploreJS/JS Problems/wordCount.js
@@ -0,0 +1,10 @@
+var speech = "I am a  good boy.";
+var count = 0;
+for(var i=0; i<speech.length; i++){
+    var char = speech[i];
+    if(char == " " && speech[i-1] != " "){
+        count++;
+    }
+}
+count++;
+console.log(count);
