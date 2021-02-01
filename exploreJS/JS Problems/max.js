1135fa5a402838d9a9c5b0f5d058751d51fe8429
OahidZihad <oahidzihad1@gmail.com>
2021-02-01 15:30:01 +0600

    Find the largest element of an array

--- exploreJS/JS Problems/max.js
+++ exploreJS/JS Problems/max.js
@@ -23,3 +23,15 @@ else{
 //// type 02
 var max = Math.max(business,minister,sochib);
 console.log(max);
+
+
+///// find max num from an array
+var marks = [45, 81, 55, 33, 25, 66, 98, 49];
+var max = marks[0];
+for(var i=0; i<marks.length; i++){
+    var element = marks[i];
+    if(element > max){
+        max = element;
+    }
+}
+console.log("Highest value is: ", max);
