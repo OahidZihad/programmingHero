a0015e27a4372c440d8f69b04abea56136d564fe
OahidZihad <oahidzihad1@gmail.com>
2021-02-01 18:16:23 +0600

    Distance never be negative

--- exploreJS/Problem Solving Assignment/feetToMile.js
+++ exploreJS/Problem Solving Assignment/feetToMile.js
@@ -1,7 +1,12 @@
 // 1 feet 0.000189394 -miles-...
 function feetToMile(feet){
-    var mile = feet * 0.000189394;
-    return mile;
+    if(feet >= 0){
+        var mile = feet * 0.000189394;
+        return mile;
+    }
+    else{
+        console.log("Distance never be negative.");
+    }
 }
-var result = feetToMile(100000);
+var result = feetToMile(-100000);
 console.log(result);
